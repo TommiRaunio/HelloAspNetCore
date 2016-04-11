e7de9133d1e36be027908dea0b34ae7ec9388f0f
Tommi Raunio <tommi.raunio@solita.fi>
2016-04-11 14:01:15 +0300

    Add Logging (work-in-progress)

--- src/HelloAspNetCore/Startup.cs
+++ src/HelloAspNetCore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace HelloAspNetCore
 {
@@ -23,15 +24,27 @@ namespace HelloAspNetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-            
+
         }
 
         // Entry point for the application.
