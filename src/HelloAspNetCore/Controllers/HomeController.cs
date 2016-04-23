3d01d0074b8907d3013423a92dfad11116127dac
Tommi Raunio <tommi.raunio@gmail.com>
2016-04-23 10:09:34 +0300

    Basic routing

--- src/HelloAspNetCore/Controllers/HomeController.cs
+++ src/HelloAspNetCore/Controllers/HomeController.cs
@@ -16,26 +16,7 @@ namespace HelloAspNetCore.Controllers
         public async Task<IActionResult> Index()
         {
             //var result = await _hslRouteSolver.GetRoute(LocationEnum.Home, LocationEnum.TomminWork);
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            return View("~/Views/Pages/Home.cshtml");
         }
     }
 }
---
+++ src/HelloAspNetCore/Controllers/RouteResultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloAspNetCore.Controllers
+{
+    [Route("HaeReitti")]
+    public class RouteResultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Pages/RouteResult.cshtml");
+        }
+    }
+}
